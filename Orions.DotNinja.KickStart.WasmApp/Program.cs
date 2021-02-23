aedd8cecbc91bc773a1b48fe9fb7c8acc6e0634a
Diana <diana@orionswave.com>
2021-02-23 14:09:16 +0200

    TODOs added to help devs navigate the code and provide app tokens and Google API keys

--- Orions.DotNinja.KickStart.WasmApp/Program.cs
+++ Orions.DotNinja.KickStart.WasmApp/Program.cs
@@ -21,13 +21,12 @@ namespace Orions.DotNinja.KickStart.WasmApp
 		{
 			var apiUrl = "https://api.blazor.ninja";
 
-			// N & N Boarding
+			// TODO Paste your app token below
 			var appToken = "eyJ0eXAiOiJDU1AiLCAiZW5jIjoiUlNBX09BRVAifQ.AKlw3OJSfuEPNSzGT72CL0QQWhUQe2iLyMEv7F-q6j9M6_LLcXYNgA5b5Xl2p-6lOQUq-GAF3peBSyXR_f5ANpHinydE_MKROfrz39xiOXDO9duhVNJHUiQ6BoPzrop-nTATO5O331HDepU-pd1EzaLkUbtj8YRg2CIRhHKydaM.Q9pYG1eESeStPAq3.t1PGIguzLp7_j76FaTgR5bO4RUvslSk5eJYcGbxvLn9yj5sWqpAWYBOA_-26Xw.sJ9wUdLl8vS6uXwStXdbgA";
 
 			var builder = WebAssemblyHostBuilder.CreateDefault(args);
 			builder.RootComponents.Add<App>("app");
 
-			//builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 			builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 			builder.Services.AddStorage();
