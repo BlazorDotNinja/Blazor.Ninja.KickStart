43b86286ee7cce107c573befe3b400fcbcd38375
Diana <diana@orionswave.com>
2021-03-12 14:34:31 +0200

    Https redirection used only in release

--- Blazor.Ninja.KickStart.ServerApp/Startup.cs
+++ Blazor.Ninja.KickStart.ServerApp/Startup.cs
@@ -48,9 +48,9 @@ namespace Blazor.Ninja.KickStart.ServerApp
 				app.UseExceptionHandler("/Error");
 				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				app.UseHsts();
+                app.UseHttpsRedirection();
 			}
 
-			app.UseHttpsRedirection();
 			app.UseStaticFiles();
 
 			app.UseRouting();
--- Blazor.Ninja.KickStart.WasmServerApp/Startup.cs
+++ Blazor.Ninja.KickStart.WasmServerApp/Startup.cs
@@ -45,9 +45,9 @@ namespace Blazor.Ninja.KickStart.WasmServerApp
 				app.UseExceptionHandler("/Error");
 				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				app.UseHsts();
+                app.UseHttpsRedirection();
 			}
 
-			app.UseHttpsRedirection();
 			app.UseBlazorFrameworkFiles();
 			app.UseStaticFiles();
 
