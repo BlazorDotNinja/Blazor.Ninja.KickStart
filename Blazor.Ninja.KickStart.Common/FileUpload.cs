68f94cc9a12373a919f12d557f26749ce361c091
Sudhir Kumar <sudhir@orionswave.com>
2021-09-13 22:34:34 +0530

    feat:add-content created new pages.

---
+++ Blazor.Ninja.KickStart.Common/FileUpload.cs
@@ -0,0 +1,22 @@
+﻿using Blazor.Ninja.Common.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blazor.Ninja.KickStart.Common
+{
+    public class FileUpload : IdDataObject
+    {
+        [Required(ErrorMessage = "Please Enter Title")]
+        public string Title { get; set; }
+        public string ContentType { get; set; }
+
+        [Required(ErrorMessage = "Please Select File")]
+        public byte[] file {get; set;}
+
+
+    }
+}
